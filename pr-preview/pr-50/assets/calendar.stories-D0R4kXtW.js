f8028bf017c843fddbb604ee821f57a26d83067f
dorianvp <dorianvp@users.noreply.github.com>
2026-09-24 17:29:13 +0000

    Deploy preview for PR 50 🛫

---
+++ pr-preview/pr-50/assets/calendar.stories-D0R4kXtW.js
@@ -0,0 +1,6 @@
+import{C as e}from"./calendar-C_Z7ZBxF.js";import"./iframe-CqJNir_t.js";import"./preload-helper-PPVm8Dsz.js";import"./utils-DCADjnpI.js";import"./button-CU1y8RHw.js";import"./index-DDlPZ3DM.js";import"./addYears-Cm1OKrbP.js";import"./createReactComponent-i9SUfz8X.js";import"./IconChevronDown-vfILwS1a.js";const c={component:e},r={args:{mode:"single",showOutsideDays:!0}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    mode: "single",
+    showOutsideDays: true
+  }
+}`,...r.parameters?.docs?.source}}};const u=["Default"];export{r as Default,u as __namedExportsOrder,c as default};
---
+++ pr-preview/pr-50/assets/index-CaQASp_V.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-CqJNir_t.js";import{r as o}from"./index-BXVgU7b0.js";var t=o();const m=r(t);export{m as R,t as r};
